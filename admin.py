2848b3ae180b16394387b9c2b9a4be44fe34fd04
南殳 <180820696@qq.com>
2026-08-16 18:26:00 +0800

    准备部署上线

--- admin.py
+++
@@ -1,8 +0,0 @@
-from blog_4 import app, db, User
-from werkzeug.security import generate_password_hash
-
-with app.app_context():
-    user = User.query.filter_by(username='南殳').first()
-    user.is_admin = True
-    db.session.commit()
-    print("✅ 管理员权限已授予")
